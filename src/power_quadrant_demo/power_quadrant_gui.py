f5fa597bf542f2214db7f11d36a08be630713582
ddavis <danieldavis998@gmail.com>
2024-04-18 16:42:19 +1200

    Extend and refactor waveform plots

--- src/power_quadrant_demo/power_quadrant_gui.py
+++ src/power_quadrant_demo/power_quadrant_gui.py
@@ -1,10 +1,12 @@
 import abc
+import cmath
 import logging
 import tkinter as tk
 from tkinter import ttk
 
 import numpy as np
 from matplotlib import patches
+from matplotlib import ticker
 from matplotlib.backend_bases import MouseButton
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg)
@@ -23,6 +25,7 @@ class GUIBlock(abc.ABC):
     style.configure('Header.TLabel', font=('Helvetica', 14))
     style.configure('SubHeader.TLabel', font=('Helvetica', 12))
     style.configure('Text.TLabel', font=('Helvetica', 12))
+    style.configure('Text.TRadiobutton', font=('Helvetica', 12))
 
     phi = tk.DoubleVar(root, name='Phi', value=0.)
     voltage = tk.DoubleVar(root, name='VoltageRMS', value=1.)
@@ -48,7 +51,13 @@ class GUIBlock(abc.ABC):
     @classmethod
     def init(cls):
         for inst in cls._gui_block_instances:
-            inst.setup()
+            inst._setup()
+
+    def _setup(self):
+        """
+        Method to allow metaclasses to control the setup workflow
+        """
+        self.setup()
 
     def setup(self):
         """
@@ -61,15 +70,20 @@ class GraphBlock(GUIBlock, metaclass=abc.ABCMeta):
     An abstract base class for GUI blocks which are matplotlib graphs
     """
 
-    def __init__(self, parent):
-        super().__init__(parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.canvas = None
         self.transient_plot_objects = []
 
-        self.setup()
-        self._refresh()
         self.phi.trace_add('write', self._refresh)
+
+    def _setup(self, *_):
+        super()._setup()
+
+        self.refresh()
+
+        self.canvas.draw()
 
     def _refresh(self, *_):
         for _ in range(len(self.transient_plot_objects)):
@@ -89,21 +103,23 @@ class GraphBlock(GUIBlock, metaclass=abc.ABCMeta):
         return canvas
 
     @abc.abstractmethod
-    def refresh(self):
-        pass
+    def setup(self): ...
+
+    @abc.abstractmethod
+    def refresh(self): ...
 
 
 class QuadrantViewer(GraphBlock):
-    DIMENSIONS = 5, 5
+    DIMENSIONS = 7, 7
 
-    def __init__(self, parent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.ax = None
         self._button_held = False
-        self.phi_str = tk.StringVar(name='PhiStr', value=f"{self.phi.get():5.2f}")
+        self.phi_str = tk.StringVar(name='PhiStr', value=f"{self.phi.get():z5.2f}")
 
-        self.phi.trace_add('write', lambda *_: self.phi_str.set(f"{self.phi.get():5.2f}"))
-
-        super().__init__(parent)
+        self.phi.trace_add('write', lambda *_: self.phi_str.set(f"{self.phi.get():z5.2f}"))
 
     def setup(self):
         fig = Figure(figsize=self.DIMENSIONS, dpi=100)
@@ -112,21 +128,27 @@ class QuadrantViewer(GraphBlock):
         self.canvas.mpl_connect('button_release_event', self._button_release_handler)
         self.canvas.mpl_connect('motion_notify_event', self._motion_notify_handler)
 
-        self.ax = fig.add_subplot(111)
+        self.ax = fig.add_subplot(111, xticks=[0], yticks=[0])
+
+        self.ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+        self.ax.xaxis.set_minor_formatter('{x:.1f}')
+
+        self.ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+        self.ax.yaxis.set_minor_formatter('{x:.1f}')
+
+        self.ax.grid(visible=True)
 
         # Plot lines on graph
         phi_array = np.linspace(0, 2 * np.pi, 100)
-        self.ax.axvline(0, -1.1, 1.1, color='k', linewidth=1)
-        self.ax.axhline(0, -1.1, 1.1, color='k', linewidth=1)
         self.ax.plot(np.sin(phi_array), np.cos(phi_array), 'k')
 
         self.ax.annotate('+P', xy=(0.94, 0.51), xycoords='axes fraction')
         self.ax.annotate('-P', xy=(0.01, 0.51), xycoords='axes fraction')
-        self.ax.annotate('+Q  (OverExcited)', xy=(0.43, 0.96), xycoords='axes fraction')
-        self.ax.annotate('-Q  (UnderExcited)', xy=(0.45, 0.02), xycoords='axes fraction')
+        self.ax.annotate('+Q  (OverExcited)', xy=(0.45, 0.96), xycoords='axes fraction')
+        self.ax.annotate('-Q  (UnderExcited)', xy=(0.46, 0.02), xycoords='axes fraction')
 
-        self.ax.set_xlim(-1.2, 1.2)
-        self.ax.set_ylim(-1.2, 1.2)
+        self.ax.set_xlim(-1.4, 1.4)
+        self.ax.set_ylim(-1.4, 1.4)
 
     def _button_press_handler(self, event):
         if event.button == MouseButton.LEFT:
@@ -150,14 +172,14 @@ class QuadrantViewer(GraphBlock):
     def refresh(self):
         apparent_power = self.voltage.get() * self.current.get()
 
-        arc_radius = 0.2
+        arc_radius = 0.1
         if apparent_power > arc_radius:
             # Plot angle arc
             angle_deg = np.rad2deg(self.phi.get())
             theta1, theta2 = 0, angle_deg
             if angle_deg < 0:
                 theta1, theta2 = theta2, theta1
-            e2 = patches.Arc((0, 0), 0.2, 0.2,
+            e2 = patches.Arc((0, 0), arc_radius * 2, arc_radius * 2,
                              theta1=theta1, theta2=theta2, linewidth=1, color='gray', alpha=0.8)
             self.transient_plot_objects.append(e2)
             self.ax.add_patch(e2)
@@ -176,7 +198,7 @@ class WaveformViewer(GraphBlock):
     """
     The section of the GUI which displays the
     """
-    DIMENSIONS = 5, 5
+    DIMENSIONS = 6, 9
     MIN_TIME, MAX_TIME = -10, 30
     PERIOD = 20
     OMEGA = 2 * np.pi / PERIOD
@@ -184,58 +206,95 @@ class WaveformViewer(GraphBlock):
     SQRT_TWO = 2 ** 0.5
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.upper_ax = None
+        self.middle_ax = None
         self.lower_ax = None
 
         self.time_array = np.linspace(self.MIN_TIME, self.MAX_TIME, 100)
-        self.voltage_wave = self.SQRT_TWO * np.sin(self.OMEGA * self.time_array)
-
-        super().__init__(*args, **kwargs)
+        self.phase_array = np.sin(self.OMEGA * self.time_array) + 1j * np.cos(self.OMEGA * self.time_array)
 
     def setup(self):
         fig = Figure(figsize=self.DIMENSIONS, dpi=100)
         self.canvas = self.create_canvas(fig)
 
-        self.upper_ax = fig.add_subplot(211)
-        self.lower_ax = fig.add_subplot(212)
+        peak_power = (self.SQRT_TWO * self.voltage.get()) * (self.SQRT_TWO * self.current.get())
 
-        self.upper_ax.plot(self.time_array, self.voltage_wave, 'r', label='Voltage', zorder=3)
+        self.upper_ax, self.middle_ax, self.lower_ax = fig.subplots(
+            nrows=3, ncols=1, sharex=True, sharey=True,
+            subplot_kw=dict(xticks=[0], yticks=[0],
+                            xlim=(self.MIN_TIME, self.MAX_TIME),
+                            ylim=(-peak_power * 1.1, peak_power * 1.1)),
+        )
+
+        self.upper_ax.xaxis.set_minor_locator(ticker.MultipleLocator(5))
+        self.upper_ax.xaxis.set_minor_formatter('{x:.0f}')
+
+        self.upper_ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+        self.upper_ax.yaxis.set_minor_formatter('{x:.1f}')
+
+        self.upper_ax.grid(visible=True)
+        self.middle_ax.grid(visible=True)
+        self.lower_ax.grid(visible=True)
+
+        self.upper_ax.set_title('Voltage/Current Waveforms')
+        self.middle_ax.set_title('Current Decomposition')
+        self.lower_ax.set_title('Power Decomposition')
+
+        self.lower_ax.set_xlabel('Time (ms)')
 
         self.refresh()
 
         self.upper_ax.legend(loc='upper right')
+        self.middle_ax.legend(loc='upper right')
         self.lower_ax.legend(loc='upper right')
 
     def refresh(self):
-        # Plot waveforms on the upper axis
         power_sign = np.sign(np.cos(self.phi.get()))
-        voltage_wave = self.SQRT_TWO * np.sin(self.OMEGA * self.time_array)
-        current_wave = self.SQRT_TWO * np.sin(self.OMEGA * self.time_array - self.phi.get()) * self.current.get()
 
+        # Convert voltage/current rms values to phasors
+        voltage_phasor = self.SQRT_TWO * self.voltage.get() + 0j
+        current_phasor = self.SQRT_TWO * self.current.get() * cmath.rect(1., self.phi.get())
+
+        # Build waveforms from phasors
+        voltage_wave = np.real(voltage_phasor * self.phase_array)
+        current_wave = np.real(current_phasor * self.phase_array)
+
+        active_current_wave = np.real(np.real(current_phasor) * self.phase_array)
+        reactive_current_wave = np.real(np.imag(current_phasor) * self.phase_array * 1j)
+        summed_current_wave = active_current_wave + reactive_current_wave
+
+        active_power_wave = voltage_wave * active_current_wave
+        reactive_power_wave = voltage_wave * reactive_current_wave
+        apparent_power_wave = voltage_wave * current_wave
+
+        # Plot waveforms on the upper axis
+        voltage_plot = self.upper_ax.plot(self.time_array, voltage_wave,
+                                          'r', label='Voltage', zorder=3)[0]
         current_plot = self.upper_ax.plot(self.time_array, current_wave,
                                           'g', label='Current', alpha=1.0 if power_sign > 0 else 0.3)[0]
-        current_alpha_plot = self.upper_ax.plot(self.time_array, -current_wave,
-                                                'g', alpha=0.3 if power_sign > 0 else 1.0)[0]
-        self.transient_plot_objects.extend([current_plot, current_alpha_plot])
+        current_inverse_plot = self.upper_ax.plot(self.time_array, -current_wave,
+                                                  'g', alpha=0.3 if power_sign > 0 else 1.0)[0]
+        self.transient_plot_objects.extend([voltage_plot, current_plot, current_inverse_plot])
 
-        # Plot waveforms on the lower axis
-        apparent_power = voltage_wave * current_wave
-        # https://www.electronics-tutorials.ws/accircuits/power-in-ac-circuits.html
+        # Plot current waveforms on the middle axis
+        active_current_plot = self.middle_ax.plot(self.time_array, active_current_wave,
+                                                  'b', label='Active Current')[0]
+        reactive_current_plot = self.middle_ax.plot(self.time_array, reactive_current_wave,
+                                                    color='orange', label='Reactive Current')[0]
+        summed_current_plot = self.middle_ax.plot(self.time_array, summed_current_wave,
+                                                  '--k', label='Apparent Current')[0]
+        self.transient_plot_objects.extend([active_current_plot, reactive_current_plot, summed_current_plot])
 
-        # apparent_power_peak = self.SQRT_TWO * self.voltage.get() * self.current.get()
-        active_power_wave = apparent_power_peak * np.sin(self.period * self.time_array - self.phi.get())
+        # Plot power waveforms on the lower axis
         active_power_plot = self.lower_ax.plot(self.time_array, active_power_wave,
                                                'b', label='Active Power')[0]
-
-        reactive_power_wave = apparent_power_peak * np.cos(self.period * self.time_array - self.phi.get())
         reactive_power_plot = self.lower_ax.plot(self.time_array, reactive_power_wave,
                                                  color='orange', label='Reactive Power')[0]
-
-        summed_power_wave = active_power_wave + reactive_power_wave
-        summed_power_plot = self.lower_ax.plot(self.time_array, summed_power_wave,
-                                               color='k', label='Apparent Power')[0]
-
-        self.transient_plot_objects.extend([active_power_plot, reactive_power_plot, summed_power_plot])
+        apparent_power_wave = self.lower_ax.plot(self.time_array, apparent_power_wave,
+                                                 '--k', label='Apparent Power')[0]
+        self.transient_plot_objects.extend([active_power_plot, reactive_power_plot, apparent_power_wave])
 
 
 class GraphOptionsPane(GUIBlock):
@@ -246,14 +305,9 @@ class GraphOptionsPane(GUIBlock):
         self.voltage.trace_add('write', self._calculate_variables)
         self.current.trace_add('write', self._calculate_variables)
 
-        phi_scale_frame = ttk.Frame(self.frame)
-        phi_scale_frame.pack(
-            side=tk.TOP, fill=tk.BOTH, expand=False)
-        phi_scale_frame.columnconfigure(2, weight=1)
-
         variables_frame = ttk.Frame(self.frame)
         variables_frame.pack(
-            side=tk.TOP, fill=tk.BOTH, expand=False)
+            side=tk.TOP, fill=tk.BOTH, expand=True)
 
         self.voltage_str = tk.StringVar(name='Vrms')
         self.current_str = tk.StringVar(name='Irms')
@@ -265,13 +319,42 @@ class GraphOptionsPane(GUIBlock):
         self.reactive_power_str = tk.StringVar(name='Q')
         row_2 = [self.apparent_power_str, self.active_power_str, self.reactive_power_str]
 
-        for i, row in enumerate([row_1, row_2]):
-            for j, var in enumerate(row):
-                ttk.Label(variables_frame, text=f'{var._name} =', style='Text.TLabel').grid(
-                    row=i, column=2 * j, sticky=tk.W, padx=(10, 0), pady=10)
-                ttk.Label(variables_frame, textvariable=var, style='Text.TLabel', width=4, anchor='e').grid(
-                    row=i, column=2 * j + 1, sticky=tk.W, padx=(0, 10), pady=10)
+        column_count = max(len(row_1), len(row_2))
 
+        for j in range(column_count):
+            col_frame = ttk.Frame(variables_frame)
+            col_frame.grid(
+                row=0, column=j, sticky=tk.EW, padx=5, pady=5)
+
+            for i, row in enumerate([row_1, row_2]):
+                if i >= len(row):
+                    continue
+
+                ttk.Label(col_frame, text=f'{row[j]._name}', style='Text.TLabel').grid(
+                    row=i, column=0, sticky=tk.W, padx=(5, 0), pady=5)
+                ttk.Label(col_frame, text=f'=', style='Text.TLabel').grid(
+                    row=i, column=1, sticky=tk.W, padx=0, pady=5)
+                ttk.Label(col_frame, textvariable=row[j], style='Text.TLabel', width=4, anchor='e').grid(
+                    row=i, column=2, sticky=tk.W, padx=(0, 5), pady=5)
+
+        pf_convention_frame = ttk.Frame(self.frame)
+        pf_convention_frame.pack(
+            side=tk.TOP, fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        # TODO: Review http://sunspec.org/wp-content/uploads/2016/08/DERPowerValueEncodingv6.pdf
+        self.pf_sign_convention = tk.StringVar(value='EEI', name='SignConvention')
+        ttk.Label(pf_convention_frame, text='PF Sign Convention:', style='Text.TLabel').pack(
+            side=tk.LEFT, fill=tk.BOTH, expand=False
+        )
+        for sign_convention in ['EEI', 'IEC']:
+            ttk.Radiobutton(pf_convention_frame,
+                            text=sign_convention,
+                            value=sign_convention,
+                            variable=self.pf_sign_convention,
+                            style='Text.TRadiobutton').pack(
+                side=tk.LEFT, fill=tk.BOTH, expand=False, padx=10, pady=5)
+
+    def setup(self):
         self._calculate_variables()
 
     def _calculate_variables(self, *_):
@@ -296,18 +379,22 @@ class PowerQuadrantsGUI(GUIBlock):
 
         self.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        graph_block = ttk.Frame(self.frame)
-        graph_block.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        left_block = ttk.Frame(self.frame)
+        left_block.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-        QuadrantViewer(graph_block).pack(side=tk.LEFT, fill=tk.BOTH, expand=False, padx=5, pady=5)
-        WaveformViewer(graph_block).pack(side=tk.LEFT, fill=tk.BOTH, expand=False, padx=5, pady=5)
-        GraphOptionsPane(self.frame).pack(side=tk.TOP, fill=tk.BOTH, expand=False, padx=5, pady=5)
+        QuadrantViewer(left_block).pack(side=tk.TOP, fill=tk.BOTH, expand=False, padx=5, pady=5)
+        GraphOptionsPane(left_block).pack(side=tk.TOP, fill=tk.BOTH, expand=False, padx=5, pady=5)
+        WaveformViewer(self.frame).pack(side=tk.LEFT, fill=tk.BOTH, expand=False, padx=5, pady=5)
+
+    def init(self):
+        super().init()
 
         self.root.resizable(False, False)
         self.root.eval('tk::PlaceWindow . center')
         self.root.minsize(self.root.winfo_width(), self.root.winfo_height())
 
     def mainloop(self):
+        self.init()
         self.root.mainloop()
 
 
